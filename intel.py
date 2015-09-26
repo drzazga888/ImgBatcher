4f8a075d988a36102b5ceb46d3e1156059227a10
drzazga888 <drzazga888@gmail.com>
2015-09-26 21:21:18 +0200

    zapis i wczytywanie ustawień (ogólnie)

--- intel.py
+++ intel.py
@@ -1,3 +1,5 @@
+import json
+
 __author__ = 'drzazga888'
 
 from PIL import Image
@@ -19,9 +21,19 @@ class Batcher:
 
     def __init__(self, images):
         self.images = images
+        self.prop = {}
 
-    def load_prop(self):
-        pass
+    def load_prop(self, path):
+        file = open(path, "r")
+        prop_encoded = file.read()
+        self.prop = json.loads(prop_encoded)
+        file.close()
+
+    def save_prop(self, path):
+        file = open(path, "w")
+        prop_encoded = json.dumps(self.prop)
+        file.write(prop_encoded)
+        file.close()
 
     def perform(self):
         pass
@@ -29,11 +41,23 @@ class Batcher:
 
 class Renamer(Batcher):
 
+    def __init__(self, images):
+        super().__init__(images)
+        super().prop = {
+            'renamer1': 'renamer2'
+        }
+
     def perform(self):
         pass
 
 
 class Resizer(Batcher):
+
+    def __init__(self, images):
+        super().__init__(images)
+        super().prop = {
+            'batcher1': 'batcher2'
+        }
 
     def perform(self):
         pass
