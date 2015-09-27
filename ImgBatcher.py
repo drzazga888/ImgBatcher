0d42753dac304cd03fc6af71ddddb5ae221205a8
drzazga888 <drzazga888@gmail.com>
2015-09-27 23:18:26 +0200

    działa wypisanie schematu masowej zmiany nazw

--- ImgBatcher.py
+++ ImgBatcher.py
@@ -6,10 +6,13 @@ __author__ = 'drzazga888'
 if __name__ == '__main__':
     i = Images()
     i.select_dir("/media/mario/Dokumenty/Zdjęcia/Moje prace")
-    r = Resizer(i)
+    """r = Resizer(i)
     r.start()
     while r.isAlive():
         print("wykonano " + str(r.processed_images()) + " na " + str(r.total_images()))
         time.sleep(0.05)
     r.join()
-    print("zakończono!")
+    print("zakończono!")"""
+    r = Renamer(i)
+    r.create_transformation_schema()
+    print(r.transformation_schema_str)
--- intel.py
+++ intel.py
@@ -1,23 +1,25 @@
 import json
 import os
 import threading
+
 from PIL import Image, ImageFilter
 
 __author__ = 'drzazga888'
 
 
 class Images:
-    """ klasa przechowuje ścieżkę katalogu ze zdjęciami i nazwy obrazków
+    """ klasa operuje na zbiorze obrazków
     """
 
     def __init__(self):
-        self.items = []
-        self.path = None
+        self.names = []
+        self.path = ""
 
     def select_dir(self, path):
+        self.path = path
         for f in os.listdir(path):
             if os.path.isfile(os.path.join(path, f)) and f.lower().endswith(('jpg', 'jpeg', 'png', 'bmp')):
-                self.items.append((os.path.join(path, f)))
+                self.names.append(f)
 
 
 class Batcher(threading.Thread):
@@ -42,6 +44,14 @@ class Batcher(threading.Thread):
         file.write(prop_encoded)
         file.close()
 
+    def run(self):
+        for img_name in self.images.names:
+            self.process_single(img_name)
+            self.processed += 1
+
+    def process_single(self, img_name):
+        pass
+
     def processed_images(self):
         return self.processed
 
@@ -55,15 +65,39 @@ class Renamer(Batcher):
 
     def __init__(self, images):
         super().__init__(images)
+        self.transformation_schema = []
+        self.transformation_schema_str = ""
         self.prop['destination'] = '/home/mario/PycharmProjects/ImgBatcher/renamed'
         self.prop['text'] = 'obrazek_'
         self.prop['digits'] = 3
-        self.prop['sort_by'] = 'name'
 
-    def run(self):
+    def create_transformation_schema(self):
+        self.transformation_schema = []
+        counter = 0
+        for img_name in self.images.names:
+            self.transformation_schema.append({
+                'before': img_name,
+                'after': self.prop['text'] + ("{0:0=" + str(self.prop['digits']) + "d}").format(
+                    counter) + os.path.splitext(img_name)[-1]
+            })
+            counter += 1
+        self._create_transformation_schema_str()
+
+    def _create_transformation_schema_str(self):
+        self.transformation_schema_str = ""
+        max_before_len = 0
+        for single_transformation in self.transformation_schema:
+            before_len = len(single_transformation['before'])
+            if before_len > max_before_len:
+                max_before_len = before_len
+        for single_transformation in self.transformation_schema:
+            self.transformation_schema_str += single_transformation['before'].ljust(max_before_len)
+            self.transformation_schema_str += " --> "
+            self.transformation_schema_str += single_transformation['after']
+            self.transformation_schema_str += '\n'
+
+    def process_single(self, img_name):
         pass
-        # self.images.items =
-        # for image in self.images.items:
 
 
 class Resizer(Batcher):
@@ -77,12 +111,11 @@ class Resizer(Batcher):
         self.prop['quality'] = 90
         self.prop['sharpen'] = True
 
-    def run(self):
-        for image in self.images.items:
-            image = Image.open(image)
-            name = os.path.splitext(os.path.basename(image.filename))[0]
-            if self.prop['sharpen']:
-                image = image.filter(ImageFilter.UnsharpMask(radius=2, percent=200, threshold=0))
-            image.thumbnail(self.prop['size'])
-            image.save(os.path.join(self.prop['destination'], name + '.jpg'), 'JPEG', quality=self.prop['quality'])
-            self.processed += 1
+    def process_single(self, img_name):
+        img = Image.open(os.path.join(img_name, self.images.path))
+        img_name_root = os.path.splitext(img_name)[0]
+        if self.prop['sharpen']:
+            img = img.filter(ImageFilter.UnsharpMask(radius=2, percent=200, threshold=0))
+        img.thumbnail(self.prop['size'])
+        img.save(os.path.join(self.prop['destination'], img_name_root + '.jpg'), 'JPEG',
+                 quality=self.prop['quality'])
