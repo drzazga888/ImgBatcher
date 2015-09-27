3ac4c0bb58c75465979a9aab630003d30fa45422
drzazga888 <drzazga888@gmail.com>
2015-09-27 20:50:47 +0200

    przetwarzanie można uruchomić w nowym wątku - można sprawdzać co jakiś czas ile zdjęć przetworzono

--- ImgBatcher.py
+++ ImgBatcher.py
@@ -1,4 +1,5 @@
 from intel import *
+import time
 
 __author__ = 'drzazga888'
 
@@ -6,7 +7,9 @@ if __name__ == '__main__':
     i = Images()
     i.select_dir("/media/mario/Dokumenty/Zdjęcia/Moje prace")
     r = Resizer(i)
-    r.perform()
-    r.prop['sharpen'] = False
-    r.prop['destination'] = '/home/mario/PycharmProjects/ImgBatcher/resized_nosh'
-    r.perform()
+    r.start()
+    while r.isAlive():
+        print("wykonano " + str(r.processed_images()) + " na " + str(r.total_images()))
+        time.sleep(0.05)
+    r.join()
+    print("zakończono!")
--- intel.py
+++ intel.py
@@ -1,5 +1,6 @@
 import json
 import os
+import threading
 from PIL import Image, ImageFilter
 
 __author__ = 'drzazga888'
@@ -8,6 +9,7 @@ __author__ = 'drzazga888'
 class Images:
     """ klasa przechowuje ścieżkę katalogu ze zdjęciami i nazwy obrazków
     """
+
     def __init__(self):
         self.items = []
         self.path = None
@@ -18,12 +20,15 @@ class Images:
                 self.items.append((os.path.join(path, f)))
 
 
-class Batcher:
+class Batcher(threading.Thread):
     """ klasa bazowa dla modułów operujących na obrazkach
     """
+
     def __init__(self, images):
+        super().__init__()
         self.images = images
         self.prop = {}
+        self.processed = 0
 
     def load_prop(self, path):
         file = open(path, "r")
@@ -37,13 +42,17 @@ class Batcher:
         file.write(prop_encoded)
         file.close()
 
-    def perform(self):
-        pass
+    def processed_images(self):
+        return self.processed
+
+    def total_images(self):
+        return len(self.images.items)
 
 
 class Renamer(Batcher):
     """ klasa, która zmienia nazwy obrazkom
     """
+
     def __init__(self, images):
         super().__init__(images)
         self.prop['destination'] = '/home/mario/PycharmProjects/ImgBatcher/renamed'
@@ -51,7 +60,7 @@ class Renamer(Batcher):
         self.prop['digits'] = 3
         self.prop['sort_by'] = 'name'
 
-    def perform(self):
+    def run(self):
         pass
         # self.images.items =
         # for image in self.images.items:
@@ -60,6 +69,7 @@ class Renamer(Batcher):
 class Resizer(Batcher):
     """ klasa tworzy miniatury
     """
+
     def __init__(self, images):
         super().__init__(images)
         self.prop['size'] = (256, 256)
@@ -67,11 +77,12 @@ class Resizer(Batcher):
         self.prop['quality'] = 90
         self.prop['sharpen'] = True
 
-    def perform(self):
+    def run(self):
         for image in self.images.items:
             image = Image.open(image)
             name = os.path.splitext(os.path.basename(image.filename))[0]
             if self.prop['sharpen']:
-                image = image.filter(ImageFilter.UnsharpMask(radius=2, percent=250, threshold=0))
+                image = image.filter(ImageFilter.UnsharpMask(radius=2, percent=200, threshold=0))
             image.thumbnail(self.prop['size'])
             image.save(os.path.join(self.prop['destination'], name + '.jpg'), 'JPEG', quality=self.prop['quality'])
+            self.processed += 1
